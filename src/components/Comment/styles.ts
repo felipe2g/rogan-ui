bd894ab5177cd26281d5a1aef8e91696d59d75ba
Felipe Guimarães <felipeguimaraes2406@gmail.com>
2021-04-12 13:22:50 -0300

    enhance comment styles

--- src/components/Comment/styles.ts
+++ src/components/Comment/styles.ts
@@ -1,5 +1,15 @@
 import styled from 'styled-components'
 
 export const Comment = styled.div`
-  color: red;
+  background-color: #eff0f6;
+
+  padding: 16px 24px;
+
+  border-radius: 0 32px 32px 32px;
+
+  color: #4e4b66;
+
+  font-size: 16px;
+  letter-spacing: 0.75px;
+  line-height: 28px;
 `
